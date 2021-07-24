b6e7c828e7231b6724b80262817ceb21daf7c3b9
David Gibson <EatThePath@gmail.com>
2021-07-24 17:23:23 -0500

    Added all non-piecewise functions.

--- code/lighting/lighting.h
+++ code/lighting/lighting.h
@@ -36,10 +36,13 @@ enum class Light_Type : int {
 
 
 enum tonemapper :int {
-	tnm_None = 0,
+	tnm_Linear = 0,
 	tnm_Uncharted = 1,
 	tnm_Aces = 2,
-	tnm_Aces_Approx = 3
+	tnm_Aces_Approx = 3,
+	tnm_Cineon = 4,
+	tnm_Reinhard_Jodie = 5,
+	tnm_Reinhard_Extended = 6
 };
 
 
