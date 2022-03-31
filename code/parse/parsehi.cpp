0aabb6431050c0bc9e312d74a5dc3932ef9746be
David Gibson <EatThePath@Gmail.com>
2022-03-30 21:14:30 -0500

    Implemented additional parsehi functions

--- code/parse/parsehi.cpp
+++ code/parse/parsehi.cpp
@@ -1,6 +1,28 @@
 #include "parselo.h"
 #include "parsehi.h"
 
+
+
+
+bool parse_optional_int_into(const SCP_string &fieldname, int* valuetarget)
+{
+	if(optional_string(fieldname.c_str())){
+		stuff_int(valuetarget);
+		return true;
+	}
+	return false;
+}
+
+bool parse_optional_clamped_int_into(const SCP_string &fieldname, int* valuetarget,const int min, const int max)
+{
+	if(parse_optional_int_into(fieldname,valuetarget)){
+		CLAMP(*valuetarget,min,max);
+		return true;
+	}
+	return false;
+}
+
+
 bool parse_optional_float_into(const SCP_string &fieldname, float* valuetarget)
 {
 	if(optional_string(fieldname.c_str())){
@@ -8,4 +30,48 @@ bool parse_optional_float_into(const SCP_string &fieldname, float* valuetarget)
 		return true;
 	}
 	return false;
+}
+
+bool parse_optional_clamped_float_into(const SCP_string &fieldname, float* valuetarget,const float min, const float max)
+{
+	if(parse_optional_float_into(fieldname,valuetarget)){
+		CLAMP(*valuetarget,min,max);
+		return true;
+	}
+	return false;
+}
+
+
+bool parse_optional_float_list_into(const SCP_string &fieldname, float* valuetarget,int length)
+{
+	if(optional_string(fieldname.c_str())){
+		stuff_float_list(valuetarget, length);
+		return true;
+	}
+	return false;
+}
+bool parse_optional_clamped_float_list_into(const SCP_string &fieldname, float* valuetarget,int length,const float min, const float max)
+{
+	if(parse_optional_float_list_into(fieldname,valuetarget,length)){
+		for(int i=0;i<length;i++){
+			CLAMP(valuetarget[i],min,max);
+		}
+		return true;
+	}
+	return false;
+}
+
+bool parse_optional_string_into(const SCP_string &fieldname,char *outstr, int type, int len, const char *terminators){
+	if( optional_string(fieldname.c_str()) ){
+		stuff_string(outstr, type, len,terminators);
+		return true;
+	}
+	return false;
+}
+
+void parse_required_string_into(const SCP_string &fieldname,char *outstr, int type, int len, const char *terminators)
+{
+	required_string(fieldname.c_str());
+	stuff_string(outstr, type, len,terminators);
+
 }
--- code/parse/parsehi.h
+++ code/parse/parsehi.h
@@ -1,7 +1,16 @@
 #pragma once
 
-//The immediate purpose of this file is to replace frequently repeated code constructs in table parsing code
+
+//The immediate purpose of this file is to replace frequently repeated code patterns in table parsing functons
+
 
 #include "parse/parselo.h"
+
+extern bool parse_optional_int_into(const SCP_string &fieldname, int* valuetarget);
+extern bool parse_optional_clamped_int_into(const SCP_string &fieldname, int* valuetarget,int min = 0, int max = 1);
+
 extern bool parse_optional_float_into(const SCP_string &fieldname, float* valuetarget);
-//extern bool stuff_optional(const SCP_string &fieldname, SCP_string* valuetarget,int type);
+extern bool parse_optional_clamped_float_into(const SCP_string &fieldname, float* valuetarget,const float min = 1.0f, const float max = 1.0f);
+extern bool parse_optional_clamped_float_list_into(const SCP_string &fieldname, float* valuetarget,int length,const float min = 1.0f, const float max = 1.0f);
+extern bool parse_optional_string_into(const SCP_string &fieldname, char *outstr, int type, int len, const char *terminators = nullptr);
+extern void parse_required_string_into(const SCP_string &fieldname, char *outstr, int type, int len, const char *terminators = nullptr);
