d3b3cec5d50f9fcb171244ef80c0ff1e8eecf68a
David Gibson <EatThePath@Gmail.com>
2022-11-08 15:45:44 -0600

    Version Number Bump

--- cmake/version.cmake
+++ cmake/version.cmake
@@ -7,7 +7,7 @@ if (EXISTS "${PROJECT_SOURCE_DIR}/version_override.cmake")
 endif()
 
 set_if_not_defined(FSO_VERSION_MAJOR 22200)
-set_if_not_defined(FSO_VERSION_MINOR 6)
+set_if_not_defined(FSO_VERSION_MINOR 7)
 set_if_not_defined(FSO_VERSION_BUILD 0)
 
 # A numeric revision e.g. the CI build number
