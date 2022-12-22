bcd05ba717572e60b7c960b3a22c8a20dd271b6e
David Gibson <EatThePath@Gmail.com>
2022-12-22 15:53:21 -0600

    simple tests for gvector

--- code/globalincs/generational.h
+++ code/globalincs/generational.h
@@ -1,6 +1,7 @@
 #pragma once
 
 #include <tl/optional.hpp>
+#include <vcruntime.h>
 
 using namespace std;
 using  tl::optional;
@@ -11,16 +12,89 @@ struct gIndex{
 
 
 template  <typename T>
+struct gEntry {
+	size_t generation;
+	optional<T> value;
+};
+
+template  <typename T>
 
 class gVector {
-	struct gEntry{
-		size_t generation;
-		optional<T> value;
-	};
-	vector<gEntry> storage;
+	vector<gEntry<T>> storage;
 	vector<size_t> known_empty;
 	public:
+	gVector<T>() = default;
+	gVector<T>(size_t capacity){
+		//fill the slots backwards, so popping from known empty later gets entry 0.
+		for (size_t i=capacity-1; i>=capacity;i--){
+			gEntry<T> e;
+			e.generation = 0;
+			e.value = tl::nullopt;
+			storage.push_back(e);
+			known_empty.push_back(i);
+		}
+	};
 	optional<T> operator[](gIndex i);
+	optional<T*> get_pointer(gIndex i);
 	gIndex add(T input);
 	void remove(gIndex i);
 };
+
+
+template  <typename T>
+optional<T> gVector<T>::operator[](gIndex i){
+	if (i.index >= storage.size()) {
+		return tl::nullopt;
+	}
+	if (storage[i.index].generation!= i.generation){
+		return tl::nullopt;
+	}
+	return storage[i.index].value;
+};
+template  <typename T>
+
+optional<T*> gVector<T>::get_pointer(gIndex i){
+	if (i.index >= storage.size()) {
+		return tl::nullopt;
+	}
+	if (storage[i.index].generation!= i.generation){
+		return tl::nullopt;
+	}
+	return *(storage[i.index].value);
+}
+template  <typename T>
+gIndex gVector<T>::add(T input){
+	if (known_empty.empty()) {
+		gEntry<T> n;
+		n.generation = 0;
+		n.value = input;
+		storage.push_back(n);
+		gIndex i;
+		i.index = storage.size()-1;
+		i.generation=0;
+		return i;
+		}
+	else {
+		auto i = known_empty.back();
+		storage[i].value = input;
+		storage[i].generation++;
+		gIndex r;
+		r.index = i;
+		r.generation = storage[i].generation;
+		known_empty.pop_back();
+		return r;
+	}
+}
+
+
+template  <typename T>
+void gVector<T>::remove(gIndex i){
+	if (i.index >= storage.size()) {
+		return;
+	}
+	if (storage[i.index].generation != i.generation ) {
+		return;
+	}
+	storage[i.index].value.reset();
+	known_empty.push_back(i.index);
+}
--- test/src/globalincs/test_generational.cpp
+++ test/src/globalincs/test_generational.cpp
@@ -1,15 +1,40 @@
 #include <gtest/gtest.h>
 
 #include "globalincs/generational.h"
+#include "tl/optional.hpp"
 
-TEST(GenerationalIndex, basics) {
+TEST(GenerationalIndex, access) {
 	{
-		gVector<float> ds;
-		gIndex a = ds.add(1.0f);
-		gIndex b = ds.add(2.0f);
-		gIndex c = ds.add(3.0f);
-		ds.remove(b);
-		gIndex d = ds.add(4.0f);
-		ASSERT_EQ(1, d.index);
+		gVector<float> data;
+		gIndex first = data.add(1.0f);
+		gIndex second = data.add(2.0f);
+		gIndex third = data.add(3.0f);
+		data.remove(second);
+		gIndex fourth = data.add(4.0f); 
+		auto bad_access = data[second];
+		auto good_access = data[fourth];
+		ASSERT_EQ(tl::nullopt, bad_access);
+		ASSERT_EQ(4.0f, good_access);
+	}
+}
+TEST(GenerationalIndex, indexes) {
+	{
+		gVector<float> data;
+		gIndex first = data.add(1.0f);
+		gIndex second = data.add(2.0f);
+		gIndex third = data.add(3.0f);
+		data.remove(second);
+		gIndex fourth = data.add(4.0f);
+		gIndex fifth = data.add(5.0f);
+		ASSERT_EQ(0, first.index);
+		ASSERT_EQ(0, first.generation);
+		ASSERT_EQ(1, second.index);
+		ASSERT_EQ(0, second.generation);
+		ASSERT_EQ(2, third.index);
+		ASSERT_EQ(0, third.generation);
+		ASSERT_EQ(1, fourth.index);
+		ASSERT_EQ(1, fourth.generation);
+		ASSERT_EQ(3, fifth.index);
+		ASSERT_EQ(0, fifth.generation);
 	}
 }
