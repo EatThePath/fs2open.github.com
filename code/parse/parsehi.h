7a487737efdb8c18c4989e240523be3beeeadd13
EatThePath <eatthepath@gmail.com>
2025-01-19 13:51:55 -0500

    New parse path

--- code/parse/parsehi.h
+++ code/parse/parsehi.h
@@ -4,6 +4,21 @@
 
 //parsehi is intended for higher level, frequently used combinations of parselo functions
 //and also for collection of standardized parsing of complex types where appropriate
+enum PARSE_MODES{
+    FLOAT,
+    BOOL
+};
+enum PARSE_REQUIREMENT{
+    OPTIONAL,
+    REQUIRED
+};
+struct parse_map_field{
+    SCP_string label;
+    size_t target_offset;
+    PARSE_MODES mode;
+    PARSE_REQUIREMENT required;
+}
+typedef parse_map SCP_unordered_map<SCP_string,parse_map_field>;
 
 extern bool parse_optional_float_into(const SCP_string& field_name, float* value_target);
 
